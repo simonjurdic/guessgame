27199abb00eb83c4cbd5a183eb1cec46f32f5e3a
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2021-11-16 09:25:47 +0100

    guna

--- guess game/Program.cs
+++ guess game/Program.cs
@@ -6,7 +6,49 @@ namespace guess_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+           Random random = new Random();
+           int nahodneCislo;
+           bool neuhodl = true;
+           int hadaneCislo;
+           nahodneCislo = random.Next(1, 100);
+
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("guess game");
+            Console.WriteLine("myslím si číslo od 1 do 100");
+            
+            while(neuhodl)
+            {
+                Console.WriteLine("tipni si číslo");
+                hadaneCislo = int.Parse(Console.ReadLine());
+                if (hadaneCislo < nahodneCislo)
+                {
+                    Console.WriteLine("číslo je větší");
+                }
+                else if (hadaneCislo > nahodneCislo) 
+                {
+                    Console.WriteLine("číslo je menší");
+                }
+                else
+                {
+                    Console.WriteLine("výborně, uhádl jsi");
+                    neuhodl = false;
+                }
+            }
+
+            
+
+
+               
+
+            
+
+
+
+
         }
     }
 }
